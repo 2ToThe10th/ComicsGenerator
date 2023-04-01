c0dcf3df65ae40311d69dd6e104f748c5db3ee72
2ToThe10th <grib.sasha2001@gmail.com>
2023-04-01 16:55:39 +0200

    fix

--- add_prompt_to_picture.py
+++ add_prompt_to_picture.py
@@ -49,44 +49,18 @@ def rounded_rectangle(
     return image
 
 
-def main1():
-    print("Hello")
+def find_place_for_phrase():
+    return (50, 50)
 
-    image = cv2.imread("img.png")
 
-    text = 'OpenCV'
-
-    # Window name in which image is displayed
-    window_name = 'Image'
-
-    # font
+def add_prompt_to_picture(image: np.ndarray, face_position: FacePosition, phrase: str) -> None:
     font = cv2.FONT_HERSHEY_SIMPLEX
-
-    # org
-    org = (50, 50)
-
-    # fontScale
-    fontScale = 1
-
-    # Blue color in BGR
-    color = (0, 0, 0)
-
-    # Line thickness of 2 px
+    org = find_place_for_phrase()
     thickness = 1
+    font_scale = 0.5
 
-    x = cv2.getTextSize(text, fontScale=fontScale, fontFace=font, thickness=thickness)
+    x = cv2.getTextSize(phrase, fontScale=font_scale, fontFace=font, thickness=thickness)
     print(x)
 
     image = rounded_rectangle(image, org, (org[0] + x[0][0], org[1] - x[0][1]), radius=20, thickness=2)
-    image = cv2.putText(image, text, org, font, fontScale, color, thickness, cv2.LINE_AA)
-
-    cv2.imwrite("out.png", image)
-
-def find_place_for_phrase():
-
-    pass
-def add_prompt_to_picture(image: np.ndarray, face_position: FacePosition, phrase: str) -> None:
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-
-
+    image = cv2.putText(image, phrase, org, font, font_scale, (0, 0, 0), thickness, cv2.LINE_AA)
--- main.py
+++ main.py
@@ -12,11 +12,14 @@ from found_face_position import found_face_position
 def get_next_comics_panel(panel_situation: str, phrase: str) -> np.ndarray:
     image = get_image_by_situation(panel_situation, style="cyberpunk")
     print(type(image))
-    face_position = found_face_position(image)
-    add_prompt_to_picture(image, face_position, phrase)
+    if phrase is not None:
+        face_position = found_face_position(image)
+        add_prompt_to_picture(image, face_position, phrase)
+    return image
 
 
 def get_image_by_situation(situation_description: str, style: str) -> np.ndarray:
+    return cv2.imread("img.png")
     result = openai.Image.create(prompt=f"Make photo in {style} style: {situation_description}", n=1, size="1024x1024")
     print(result)
     if "error" in result:
@@ -28,11 +31,11 @@ def get_image_by_situation(situation_description: str, style: str) -> np.ndarray
     return cv2.imdecode(image_in_array, -1)  # 'Load it as it is'
 
 
-
 def main():
     # TODO: get panels from chatgpt
     image = get_next_comics_panel(
-        "The man walks into a T-Mobile store, where he is greeted by a friendly sales representative.", "Welcome to T-Mobile! How can I assist you today?"
+        "A man with messy hair and glasses is seen walking on the street while staring at his old and outdated phone. He looks frustrated and annoyed.",
+        "Ugh, this phone is driving me crazy. I need a new one",
     )
     cv2.imwrite("out.png", image)
 
